eae2abc98887be330a271d402fdd2289f2ee1f67
jankoa <andras.janko@gmail.com>
2013-11-11 16:34:10 +0100

    build fix

--- build.fsx
+++ build.fsx
@@ -21,29 +21,8 @@ do  use cl = new System.Net.WebClient()
     )
     cl.DownloadFile(
         "http://code.highcharts.com/highcharts.js",
-        "IntelliFactory.WebSharper.Highcharts/highcharts.js"
+        @"IntelliFactory.WebSharper.Highcharts\highcharts.js"
     )
-
-#I "packages/WebSharper.2.5.73.1-alpha/tools/net45"
-
-#r "IntelliFactory.WebSharper"
-#r "IntelliFactory.WebSharper.JQuery"
-#r "IntelliFactory.WebSharper.InterfaceGenerator"
-
-
-//let configsJson, membersJson =
-//    use cl = new System.Net.WebClient()
-//    cl.DownloadString "http://api.highcharts.com/highcharts/option/dump.json",
-//    cl.DownloadString "http://api.highcharts.com/highcharts/object/dump.json"
-//
-//#I "packages/FParsec.1.0.1/lib/net40-client"
-//#r "FParsec"
-//#r "FParsecCS"
-//  
-//#load "IntelliFactory.WebSharper.Highcharts/json.fs"
-//
-//let configs = Json.parse configsJson
-//let members = Json.parse membersJson
 
 let bt = 
     BuildTool().PackageId("WebSharper.Highcharts", "2.5")
